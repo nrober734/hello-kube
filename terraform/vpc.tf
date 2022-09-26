8fcdbba18d399aa27abaf191ac6a78791bb91796
Nic Roberts II <nic2@Nics-MBP.attlocal.net>
2022-09-25 17:56:42 -0700

    you couldnt pay me enough to be a fucking devops engineer fuck my life

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
   name = "hello-kube-vpc"
   cidr = "10.0.0.0/16"
-  azs = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  azs = ["${var.region}a", "${var.region}b"]
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
