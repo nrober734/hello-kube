7ce7209a0b205821baeef726635baeed5cf26006
Nic Roberts II <nic2@Nics-MBP.attlocal.net>
2022-09-25 20:16:40 -0700

    idk what the fuck is wrong but maybe deploying with only one node will help who the fuck knows

--- terraform/sec_groups.tf
+++ terraform/sec_groups.tf
@@ -9,20 +9,7 @@ resource "aws_security_group" "node_group_one" {
 
     cidr_blocks = [
       "10.0.0.0/8",
-    ]
-  }
-}
-
-resource "aws_security_group" "node_group_two" {
-  name_prefix = "node_group_two"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
+      "172.16.0.0/12",
       "192.168.0.0/16",
     ]
   }
