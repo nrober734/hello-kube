c42c248f463f924dc42e35488550c489ca93be82
Nic Roberts II <nic2@Nics-MBP.attlocal.net>
2022-09-25 21:24:28 -0700

    try fixing redudant sec group

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -56,6 +56,7 @@ module "eks" {
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
     attach_cluster_primary_security_group = true
+    create_security_group = false
     vpc_security_group_ids = [aws_security_group.node_group_one.id]
   }
 
